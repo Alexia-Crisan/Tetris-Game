16347f8d165c50fa0e30e81539bbbf4f4c07751f
Alexia-Crisan <150911612+Alexia-Crisan@users.noreply.github.com>
2025-08-04 16:39:25 +0300

    game loop created

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,18 @@
 
 int main()
 {
-    InitWindow(300, 600, "raylib Tetris");
+    Color darkBlue = {44, 44, 127, 255};
 
-    // CloseWindow();
+    InitWindow(300, 600, "Tetris");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false) // checks for pressed ESC
+    {
+        BeginDrawing();
+        ClearBackground(darkBlue);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
